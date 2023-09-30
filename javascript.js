e12a901984446725f96037005664b518363d06e3
Armando Lima <113323844+armando-lima@users.noreply.github.com>
2023-09-30 00:10:42 -0400

    Create function to ask player input

--- javascript.js
+++ javascript.js
@@ -3,3 +3,12 @@ function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[randomIndex];
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Please input your choice: Rock / Paper / Scissor").toLowerCase();
+    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissor") {
+        console.log("Wrong input. Please select one of the following: Rock / Paper / Scissor");
+        playerChoice = prompt("Please input your choice: Rock / Paper / Scissor").toLowerCase();
+    }
+    return playerChoice;
+}
